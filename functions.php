1c05380d6a58f735f7333c7399895346c3758ad6
Tushar Khurana <tkhurana463@gmail.com>
2025-06-28 23:37:48 +0530

    Add the stylesheet using functions.php

--- functions.php
+++ functions.php
@@ -4,3 +4,13 @@
  * 
  * @package tktheme
  */
+
+ function tktheme_enqueue_scripts()
+ {
+    
+    wp_enqueue_style('stylesheet', get_stylesheet_uri(), [] , filemtime( get_template_directory() . '/style.css'),'all');
+ }
+
+ add_action('wp_enqueue_scripts','tktheme_enqueue_scripts'); // on wp enqueue script hook this action will be trigerred to register style and js files
+
+ ?>
