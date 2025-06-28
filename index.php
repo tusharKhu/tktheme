913ad9299b9bf38fbb2b5d4f8239307163acefc0
Tushar Khurana <tkhurana463@gmail.com>
2025-06-28 21:28:46 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Main template file.
+ * 
+ * @package tktheme
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tushar Custom Theme</title>
+</head>
+<body>
+    
+</body>
+</html>
